1c10ea13d5f3d9f413ca55c838ab65b1b3bf125f
Az1yaT <jeenbek0vv.06@gmail.com>
2022-11-14 19:36:47 +0600

    Home Work 5 is done and start to do Home Work 6

--- Seminars/src/Sem05_HardNode.java
+++ Seminars/src/Sem05_HardNode.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Sem05_EzNode {
+public class Sem05_HardNode {
     public static void main(String[] args) {
 
         Node n1 = new Node(1);
@@ -13,6 +13,7 @@ public class Sem05_EzNode {
         Node n6 = new Node(6);
 
         Node n7 = new Node(7);
+        Node n8 = new Node(8);
         Node n9 = new Node(9);
         Node n11 = new Node(11);
         Node n20 = new Node(20);
@@ -22,6 +23,7 @@ public class Sem05_EzNode {
 
         n2.add(n4);
         n4.add(n7);
+        n4.add(n8);
         n4.add(n9);
 
         n3.add(n5);
@@ -29,15 +31,14 @@ public class Sem05_EzNode {
         n6.add(n11);
         n6.add(n20);
 
-        preOrder(n1, " ");
-
+        preOrder(n1, "");
     }
 
     static void preOrder(Node root, String sp) {
         if (root != null) {
             System.out.print(root.value + " ");
             for (Node c : root.node) {
-                preOrder(c, sp + "");
+                preOrder(c, sp + " ");
             }
         }
     }
